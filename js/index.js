1e77eeca3a64dde4ac4af59a20087d108a711e2d
Qixing Yi <112390972+MaLanhui@users.noreply.github.com>
2024-08-21 22:20:03 +0800

    Update index.js

--- js/index.js
+++ js/index.js
@@ -24,7 +24,7 @@ var S = {
     if (i !== -1) {
       S.UI.simulate(decodeURI(action).substring(i + 3));
     } else {
-      S.UI.simulate('|#countdown 3||某某某|我们|在一起吧|🎂|');
+      S.UI.simulate('|#countdown 3||罗琳|祝你|生日快乐!|🎂|');
     }
 
     S.Drawing.loop(function () {
